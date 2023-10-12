98a03c0807a64304a1fbbc89ea9b3844ce74bcce
Nohemi30 <147261816+Nohemi30@users.noreply.github.com>
2023-10-11 23:01:34 -0600

    Update Ejercicio1
    entregar

--- Ejercicio_1.cs
+++ Ejercicio_1.cs
@@ -3,3 +3,21 @@
 
 Escribe un programa
 que muestre los números pares del 2 al 20. */
+csharp
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Números pares del 2 al 20:");
+
+        for (int i = 2; i <= 20; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
+    }
+}
+
