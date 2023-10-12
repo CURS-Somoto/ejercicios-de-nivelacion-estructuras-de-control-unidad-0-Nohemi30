cd92bc9cc84a4886658caddf379862d3e5ac1369
Nohemi30 <147261816+Nohemi30@users.noreply.github.com>
2023-10-12 12:14:38 -0600

    Ejercicio_6.cs

--- Ejercicio_6.cs
+++ Ejercicio_6.cs
@@ -3,3 +3,26 @@
 
 Escribe un programa que diga si un número
 es par o impar.*/
+csharp
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar");
+        }
+
+        Console.ReadLine();
+    }
+}
+
