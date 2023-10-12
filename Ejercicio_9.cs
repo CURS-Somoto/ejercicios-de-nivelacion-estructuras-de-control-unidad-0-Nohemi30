536cbc3ea4b172396b94332bc52c0e2aab6c8ebc
Nohemi30 <147261816+Nohemi30@users.noreply.github.com>
2023-10-12 12:35:29 -0600

    Ejercicio_9.cs
    trabajo entregado

--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -6,3 +6,22 @@ Dado dos números, muestra el número mayor
 utilizando una estructura if
 
 */
+csharp
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
